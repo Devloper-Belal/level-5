79db283130a1ba7c5394d13314e0444bfcce1655
github_Devloper-Belal <asaamman123.bk@gmail.com>
2023-02-04 17:50:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const sendData=()=>{
+
+localStorage.setItem("myName","belal")
+}
+const getData=()=>{
+
+  //localStorage.setItem("myName")
+  gett.innerText=localStorage.getItem("myName");
+  }
+  
